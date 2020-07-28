0daff3402495a663be5417c9c21d43534451e745
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2020-07-28 21:02:39 +0100

    chore: Add db connect helper
    This files stores the database connection as a singleton.
    
    The app.js loads it and opens the connection before the app starts

--- api/app.js
+++ api/app.js
@@ -1,36 +1,47 @@
 'use strict';
 
+// Load env once
+require('helpers/load-env')();
+
 // Module imports
 const http = require('http');
 const express = require('express');
 const boot = require('helpers/boot');
 const apiIndex = require('routes/index');
 const log = require('helpers/logger');
+const db = require('helpers/db');
 
-// Load env once
-require('helpers/load-env')();
+db.connect(
+	(err) => {
+		if (err) {
+			log.error(err);
+			return;
+		}
 
-const app = express();
+		const app = express();
 
-app.set('title', process.env.APP_NAME || 'APP_NAME');
-app.set('env', process.env.APP_ENV || 'APP_ENV');
+		app.set('title', process.env.APP_NAME || 'APP_NAME');
+		app.set('env', process.env.APP_ENV || 'APP_ENV');
 
-// Setup app middlewares and routes
-boot(app);
+		// Setup app middlewares and routes
+		boot(app);
 
-// Default routes
-app.use('/', apiIndex);
+		// Default routes
+		app.use('/', apiIndex);
 
-const server = http.createServer(app);
+		const server = http.createServer(app);
 
-server.listen(process.env.HTTP_PORT, (err) => {
-	if (err) {
-		log.error(err);
-		return 1;
-	}
-	log.info('HTTP running on port ' + process.env.HTTP_PORT);
-});
+		server.listen(process.env.HTTP_PORT, (err) => {
+			if (err) {
+				log.error(err);
+				return 1;
+			}
+			log.info('HTTP running on port ' + process.env.HTTP_PORT);
+		});
+	},
+);
 
 
 // TODO: Tests that app mounts
 // TODO: Add health check route
+// TODO: Defer database closing
