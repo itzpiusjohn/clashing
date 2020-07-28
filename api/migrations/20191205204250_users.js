db51f00926956043c6c3443609e117512f856b36
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2020-07-28 21:14:21 +0100

    migration: Make email and mobile_number unique

--- api/migrations/20191205204250_users.js
+++ api/migrations/20191205204250_users.js
@@ -4,11 +4,17 @@ exports.up = function(knex) {
 	return knex.schema.createTable(
 		'users',
 		(table) => {
-			table.bigIncrements('id', 11).unsigned().primary();
+			table.bigIncrements('id', 11).unsigned();
 			table.string('name', 60).notNullable();
 			table.string('email', 60).notNullable();
-			table.string('mobile', 20).notNullable();
+			table.string('mobile_number', 20);
+			table.string('role', 20).defaultTo('customer').notNullable();
 			table.string('password');
+			table.timestamp('created_at', {precision: 6}).defaultTo(knex.fn.now(6)).notNullable();
+			table.timestamp('updated_at', {precision: 6}).defaultTo(knex.fn.now(6)).notNullable();
+			table.timestamp('deleted_at', {precision: 6});
+			table.unique('email', 'uk-email');
+			table.unique('mobile_number', 'uk-mobile_number');
 		},
 	);
 };
